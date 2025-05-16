64282b79d3cfd61dfa551037f550aca73353fbbe
Victor-Estrella <victorhcarracci@gmail.com>
2025-05-16 10:31:35 -0300

    Estilização nos botões da pagina de configuração

--- Configuracoes.tsx
+++ Configuracoes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TextInput, Button, Alert } from 'react-native';
+import { Alert, Pressable, Text, TextInput, View } from 'react-native';
 import { styles } from './estilos';
 
 const Configuracoes = (): React.ReactElement => {
@@ -7,7 +7,7 @@ const Configuracoes = (): React.ReactElement => {
     const [email, setEmail] = React.useState('');
 
     const atualizarConta = () => {
-        // Aqui você faria a chamada para atualizar os dados do usuário
+        // Código para atualizar os dados do usuário
         Alert.alert('Conta atualizada!', 'Suas informações foram atualizadas com sucesso.');
     };
 
@@ -17,8 +17,9 @@ const Configuracoes = (): React.ReactElement => {
             'Tem certeza que deseja deletar sua conta? Esta ação não pode ser desfeita.',
             [
                 { text: 'Cancelar', style: 'cancel' },
-                { text: 'Deletar', style: 'destructive', onPress: () => {
-                        // Aqui você faria a chamada para deletar a conta do usuário
+                {
+                    text: 'Deletar', style: 'destructive', onPress: () => {
+                        // Código para deletar a conta do usuário
                         Alert.alert('Conta deletada', 'Sua conta foi deletada.');
                     }
                 },
@@ -29,15 +30,31 @@ const Configuracoes = (): React.ReactElement => {
     return (
         <View style={styles.containerConfig}>
             <Text style={styles.tituloConfig}>Configurações da Conta</Text>
-            <TextInput style={styles.inputConfig} placeholder="Nome" value={nome} onChangeText={setNome} placeholderTextColor="white"/>
-            <TextInput style={styles.inputConfig} placeholder="E-mail" value={email} onChangeText={setEmail} placeholderTextColor="white" keyboardType="email-address" autoCapitalize="none"/>
-            <Button title="Atualizar Conta" onPress={atualizarConta} />
+            <TextInput style={styles.inputConfig} placeholder="Nome" value={nome} onChangeText={setNome} placeholderTextColor="white" />
+            <TextInput style={styles.inputConfig} placeholder="E-mail" value={email} onChangeText={setEmail} placeholderTextColor="white" keyboardType="email-address" autoCapitalize="none" />
             <View style={styles.deleteConfig}>
-                <Button title="Deletar Conta" color="#d9534f" onPress={deletarConta}/>
+                <Botao title="Atualizar Conta" color='#0CB0E7FF' onPress={atualizarConta} />
+                <Botao title="Deletar Conta" color='#d9534f' onPress={deletarConta} />
             </View>
         </View>
     );
 };
 
+
+interface BotaoPropsConfig {
+    title: string
+    color: string 
+    onPress: () => void
+}
+
+function Botao(props: BotaoPropsConfig) {
+    return (
+        <Pressable onPress={props.onPress}>
+            <View style={{backgroundColor: props.color, borderRadius: 16, marginHorizontal: 12 }}>
+                <Text style={styles.botaoTexto}>{props.title}</Text>
+            </View>
+        </Pressable>
+    );
+}
 
 export default Configuracoes;
--- estilos.ts
+++ estilos.ts
@@ -1,157 +1,159 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'stretch',
-      justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
 
-    // Formulário de Autenticação (Cadastro e Login)
-    viewInputAutenticacao: {
-      marginTop:40,
-      marginBottom: 12,
-    }, 
-    inputAutenticacao: {
-      backgroundColor:'gray', 
-      borderRadius: 7,
-      padding: 4,
-      color: 'white',
-      fontSize: 22,
-      fontWeight: 300,
-    },
-    tituloAutenticacao: {
-      fontSize:48, 
-      fontWeight: 'bold', 
-      marginTop: 32, 
-      color:'#00b131'
-    },
-    buttonTextAutenticacao: { 
-      fontSize: 30, 
-      fontWeight: "bold", 
-      textAlign: "center",
-      padding: 10,
-      color: "white",
-    },
+  // Formulário de Autenticação (Cadastro e Login)
+  viewInputAutenticacao: {
+    marginTop: 40,
+    marginBottom: 12,
+  },
+  inputAutenticacao: {
+    backgroundColor: 'gray',
+    borderRadius: 7,
+    padding: 4,
+    color: 'white',
+    fontSize: 22,
+    fontWeight: 300,
+  },
+  tituloAutenticacao: {
+    fontSize: 48,
+    fontWeight: 'bold',
+    marginTop: 32,
+    color: '#00b131'
+  },
+  buttonTextAutenticacao: {
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+    padding: 10,
+    color: "white",
+  },
 
-    // Formulário de Cadastro de Motos
-    viewInputFormulario: {
-      flexDirection: 'column',
-      marginTop: 12,
-    },
-    input: {
-      backgroundColor: '#5D5D5D',
-      borderRadius: 8,
-      padding: 4,
-      color: 'white',
-      fontSize: 18,
-    },
-    labelFormulario: {
-      color: 'white',
-      fontSize: 22,
-      fontWeight: 200,
-    },
-    tituloFormulario: {
-      fontSize: 32,
-      fontWeight: 'bold',
-      marginVertical: 24,
-      color: '#00b131',
-      textAlign: 'center',
-    },
-    botao: {
-      borderRadius: 16, 
-      marginTop: 42, 
-      backgroundColor: 'green'
-    },
-    botaoTexto: { 
-      fontSize: 24, 
-      fontWeight: "bold", 
-      textAlign: "center",
-      padding: 10,
-      color: "white",
-    },
-    
+  // Formulário de Cadastro de Motos
+  viewInputFormulario: {
+    flexDirection: 'column',
+    marginTop: 12,
+  },
+  input: {
+    backgroundColor: '#5D5D5D',
+    borderRadius: 8,
+    padding: 4,
+    color: 'white',
+    fontSize: 18,
+  },
+  labelFormulario: {
+    color: 'white',
+    fontSize: 22,
+    fontWeight: 200,
+  },
+  tituloFormulario: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginVertical: 24,
+    color: '#00b131',
+    textAlign: 'center',
+  },
+  botao: {
+    borderRadius: 16,
+    marginTop: 42,
+    backgroundColor: 'green'
+  },
+  botaoTexto: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+    padding: 10,
+    color: "white",
+  },
 
-    // Listagem de Motos
-    tabelaContainer: {
-      padding: 16,
-      minWidth: 400
-    },
-    tituloTabela: {
-      fontSize: 22,
-      fontWeight: "bold",
-      marginBottom: 12,
-      textAlign: "center",
-      color: "#00b131"
-    },
-    cabecalhoTabela: {
-      flexDirection: "row",
-      backgroundColor: "#eee",
-      paddingVertical: 8,
-      borderBottomWidth: 1,
-      borderColor: "#ccc",
-    },
-    cabecalhoTextoTabela: {
-      flex: 1,
-      fontWeight: "bold",
-      textAlign: "center",
-    },
-    linhaTabela: {
-      flexDirection: "row",
-      borderBottomWidth: 1,
-      borderColor: "#ccc",
-      backgroundColor: "#333",
-      paddingVertical: 8,
-    },
-    objetosTabela: {
-      flex: 1,
-      textAlign: "center",
-      color: "white",
-    },
-    pressableDetalhesTabela:{
-      flex: 1,
-      alignItems: "center",
-    },
-    botaoDetalhes: {
-      backgroundColor: "green",
-      paddingVertical: 6,
-      paddingHorizontal: 8,
-      borderRadius: 8,
-      alignItems: "center",
-    },
-    botaoTextoDetalhes: {
-      color: "#fff",
-      fontSize: 12,
-    },
-  
 
-    // Configurações
-    containerConfig: {
-      flex: 1,
-      padding: 24,
-      backgroundColor: '#000',
-      justifyContent: 'center',
-    },
-    tituloConfig: {
-      fontSize: 22,
-      fontWeight: 'bold',
-      marginBottom: 32,
-      textAlign: 'center',
-      color: '#00b131',
-    },
-    inputConfig: {
-      borderWidth: 1,
-      borderColor: '#ccc',
-      borderRadius: 8,
-      padding: 12,
-      marginBottom: 16,
-      fontSize: 16,
-      color: 'white',
-    },
-    deleteConfig: {
-      marginTop: 32,
-    },
+  // Listagem de Motos
+  tabelaContainer: {
+    padding: 16,
+    minWidth: 400
+  },
+  tituloTabela: {
+    fontSize: 22,
+    fontWeight: "bold",
+    marginBottom: 12,
+    textAlign: "center",
+    color: "#00b131"
+  },
+  cabecalhoTabela: {
+    flexDirection: "row",
+    backgroundColor: "#eee",
+    paddingVertical: 8,
+    borderBottomWidth: 1,
+    borderColor: "#ccc",
+  },
+  cabecalhoTextoTabela: {
+    flex: 1,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  linhaTabela: {
+    flexDirection: "row",
+    borderBottomWidth: 1,
+    borderColor: "#ccc",
+    backgroundColor: "#333",
+    paddingVertical: 8,
+  },
+  objetosTabela: {
+    flex: 1,
+    textAlign: "center",
+    color: "white",
+  },
+  pressableDetalhesTabela: {
+    flex: 1,
+    alignItems: "center",
+  },
+  botaoDetalhes: {
+    backgroundColor: "green",
+    paddingVertical: 6,
+    paddingHorizontal: 8,
+    borderRadius: 8,
+    alignItems: "center",
+  },
+  botaoTextoDetalhes: {
+    color: "#fff",
+    fontSize: 12,
+  },
+
+
+  // Configurações
+  containerConfig: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: '#000',
+  },
+  tituloConfig: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 32,
+    textAlign: 'center',
+    color: '#00b131',
+  },
+  inputConfig: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
+    padding: 12,
+    marginBottom: 16,
+    fontSize: 16,
+    color: 'white',
+  },
+  deleteConfig: {
+    marginTop: 32,
+    flexDirection: 'row',
+    alignSelf: 'center'
+  },
 });
 
 export { styles };
+
